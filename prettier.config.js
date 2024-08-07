771afd69b204990c705c22e6181565150ff894f2
Alvaro J. Terracciano <ajterracciano.s@gmail.com>
2024-08-06 23:46:04 -0300

    Delete prettier.config.js

--- prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  files: ['*.js', '*.ts', '*.scss', '*.html', '*.json'],
-  printWidth: 100,
-  trailingComma: 'all',
-  bracketSpacing: true,
-  tabWidth: 2,
-  semi: true,
-  singleQuote: true,
-};
